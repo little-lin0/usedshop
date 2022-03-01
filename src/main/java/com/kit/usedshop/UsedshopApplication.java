ba44a5b85c432363bd31e21413bf8f2f9ac8386e
Deviii <874583008@qq.com>
2022-03-01 17:01:07 +0800

    基本框架

---
+++ src/main/java/com/kit/usedshop/UsedshopApplication.java
@@ -0,0 +1,13 @@
+package com.kit.usedshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsedshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsedshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kit/usedshop/UsedshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kit.usedshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsedshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
